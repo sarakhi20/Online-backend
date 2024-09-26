e050700eeb8a2bc0135fe7fd8941a46ee3a50de9
sarakhi20 <sarakhiammu96@gmail.com>
2024-09-26 22:32:34 +0530

    first commit

---
+++ src/Key.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONGOURI:"mongodb+srv://VidushiGandhi:YhbuK3ov4gZHlJxG@cluster0.yzbdbwj.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"gdjhvvdhjdbvxvjLsasdhpdbcghcjcjjhjhsbshshsbwey"
+
+}
